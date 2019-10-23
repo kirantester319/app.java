b4730e915edfbb94bb0e34daf9c808825795b08d
Kiran <kirantester319@gmail.com>
2019-10-23 19:23:33 +0530

    This is modified commit

---
+++ src/main/java/Maven/Project/Strings_Removenumbers.java
@@ -0,0 +1,38 @@
+package Maven.Project;
+
+public class Strings_Removenumbers {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		String str = "This is Java Program 404";
+		
+		
+		String str2 = str.replaceAll("[0-9]", "");
+		
+		System.out.println(str2);
+		
+		String str3= "THis is Selenium Program";
+		String str4 ="";
+		
+		char[] a = str3.toCharArray();
+		
+		for(int i=0;i<a.length;i++)
+		{
+			if (a[i]!=' ')
+			{
+				
+				str4 = str4+str3.charAt(i);
+				
+			}
+		}
+		
+		System.out.println(str4);
+		
+		
+		
+		
+	}
+
+}
